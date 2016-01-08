59dc16f86b027c7d191ca4e312e8d2060f832ea7
Peter Fraedrich <peter.fraedrich@hexapp.net>
2016-01-08 11:04:49 -0500

    added python lib for SlateKV

---
+++ slatelib.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from datetime import datetime as dt
+import json
+from urllib2 import urlopen as http
+
+class slate:
+    def get(self, query, url, **kwargs):
+        if ':' in query:
+            raise TypeError('Query must be single key only!')
+        else:
+            q = url + 'get/' + query
+            try:
+                return http(q).read().strip('\n')
+            except Exception as (e):
+                raise IOError('Unable to reach SlateKV URL. Err:'  + e)
+
+    def post(self, query, url, **kwargs):
+        if len(query.split(':')) != 2:
+            raise TypeError('Object must be key:value! Not enough or too many key:value pairs submitted')
+        else:
+            q = url + 'post/' + query
+            try:
+                return http(q).read.strip('\n')
+            except Exception as (e):
+                raise IOError('Unable to reach SlateKV URL. Err:'  + e)
+
+    def change(self, query, url, **kwargs):
+        if len(query.split(':')) != 2:
+            raise TypeError('Object must be key:value! Not enough or too many key:value pairs submitted')
+        else:
+            q = url + 'change/' + query
+            try:
+                return http(q).read.strip('\n')
+            except Exception as (e):
+                raise IOError('Unable to reach SlateKV URL. Err:'  + e)
